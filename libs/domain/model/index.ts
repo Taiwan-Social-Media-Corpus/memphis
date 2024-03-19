6b31bb2cf65f96c9d4588d4949646a35bf2d5d7e
Retr0327 <retr0327.dev@gmail.com>
2024-03-19 16:54:54 +0800

    Refactor domain model imports

--- libs/domain/model/index.ts
+++ libs/domain/model/index.ts
@@ -1,44 +1,5 @@
-export abstract class Entity<Id, Props> {
-  readonly id: Id;
-  protected props: Props;
+import { Entity } from './entity';
+import { AggregateRoot } from './aggregate-root';
+import { ValueObject } from './value-object';
 
-  protected constructor(id: Id, props: Props) {
-    this.id = id;
-    this.props = props;
-  }
-
-  public abstract equals(obj?: Entity<Id, Props>): boolean;
-}
-
-export abstract class AggregateRoot<Id, Props> extends Entity<Id, Props> {}
-
-interface LiteralObject {
-  [index: string]: unknown;
-}
-
-export abstract class ValueObject<Props extends object> {
-  props: Readonly<Props>;
-
-  constructor(props: Props) {
-    this.props = Object.freeze(props);
-  }
-
-  equals(obj?: ValueObject<Props>): boolean {
-    if (obj === null || obj === undefined) {
-      return false;
-    }
-    if (obj.props === undefined) {
-      return false;
-    }
-    const shallowObjectEqual = (props1: LiteralObject, props2: LiteralObject) => {
-      const keys1 = Object.keys(props2);
-      const keys2 = Object.keys(props1);
-
-      if (keys1.length !== keys2.length) {
-        return false;
-      }
-      return keys1.every((key) => props2.hasOwnProperty(key) && props2[key] === props1[key]);
-    };
-    return shallowObjectEqual(this.props, obj.props);
-  }
-}
+export { Entity, AggregateRoot, ValueObject };
