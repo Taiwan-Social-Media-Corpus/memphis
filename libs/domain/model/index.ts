411a290b2c61348d16e109ef017d20cc3e4c484c
Retr0327 <retr0327.dev@gmail.com>
2024-03-21 22:49:57 +0800

    Define abstract domain entity

--- libs/domain/model/index.ts
+++
@@ -1,5 +0,0 @@
-import { Entity } from './entity';
-import { AggregateRoot } from './aggregate-root';
-import { ValueObject } from './value-object';
-
-export { Entity, AggregateRoot, ValueObject };
