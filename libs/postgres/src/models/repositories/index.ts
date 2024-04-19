9912172d3e8f20c7e996a1742e35eba08777b246
Retr0327 <retr0327.dev@gmail.com>
2024-04-19 21:57:45 +0800

    feat: export postgresql repositories (ref #1)

---
+++ libs/postgres/src/models/repositories/index.ts
@@ -0,0 +1,6 @@
+import UserRepository from './user.repository';
+import AdminRepository from './admin.repository';
+
+const repositories = [UserRepository, AdminRepository];
+
+export { UserRepository, AdminRepository, repositories };
