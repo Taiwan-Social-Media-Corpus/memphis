0ce760827ca3700dffa1ae9fd94b73e108691f80
Retr0327 <retr0327.dev@gmail.com>
2024-04-19 20:10:44 +0800

    refactor: replace direct repository access with service layer (ref #1)

--- apps/admin/src/application/use-cases/admin/queries/login/index.ts
+++ apps/admin/src/application/use-cases/admin/queries/login/index.ts
@@ -1,21 +1,25 @@
 import { UnauthorizedException } from '@nestjs/common';
 import { QueryHandler, IQueryHandler, EventPublisher } from '@nestjs/cqrs';
-import { AdminRepository } from '@memphis/admin/infrastructure/repositories';
+import { GetAdminService } from '@memphis/admin/application/services/admins';
 import { LoginQuery } from './query';
 
 @QueryHandler(LoginQuery)
 export class LoginQueryHandler implements IQueryHandler<LoginQuery> {
   constructor(
-    private readonly adminRepository: AdminRepository,
+    private readonly getAdminService: GetAdminService,
     private readonly eventPublisher: EventPublisher,
   ) {}
 
   async execute(query: LoginQuery) {
-    const adminEntity = await this.adminRepository.findOneByEmail(query.email);
-    if (adminEntity === null) {
+    const adminResult = await this.getAdminService.execute({
+      email: query.email,
+    });
+
+    if (adminResult.isErr()) {
       throw new UnauthorizedException();
     }
 
+    const adminEntity = adminResult.getValue();
     const pwdResult = await adminEntity.validatePassword(query.password);
     if (pwdResult.isErr()) {
       throw pwdResult.getError();
