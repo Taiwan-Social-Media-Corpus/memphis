8fd8f4fa33751a1968e61b2b8f763e94c51ec5d5
Retr0327 <retr0327.dev@gmail.com>
2024-04-20 21:49:39 +0800

    feat: implement user guard (ref #2)

---
+++ apps/corpus/src/application/services/guards/user.guard.ts
@@ -0,0 +1,55 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { UserRepository } from '@memphis/corpus/infrastructure/repositories';
+import { CookieService } from '@memphis/cookie';
+import Definition from '@memphis/definition';
+import { JwtService } from '@memphis/jwt';
+import { EventPublisher } from '@nestjs/cqrs';
+
+@Injectable()
+export class UserGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userRepository: UserRepository,
+    private readonly cookieService: CookieService,
+    private readonly eventPublisher: EventPublisher,
+  ) {}
+
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest<Request>();
+    const response = context.switchToHttp().getResponse<Response>();
+    const cookies = request.context.getCookies();
+    const jwtCookie = cookies.get(Definition.Cookies.authToken, {
+      signed: true,
+    });
+    if (!jwtCookie) {
+      return false;
+    }
+
+    const jwt = await this.jwtService.verify(jwtCookie);
+    if (!jwt) {
+      throw new UnauthorizedException('JWT expired');
+    }
+    const userEntity = await this.userRepository.findOne({
+      id: jwt.payload.uid,
+    });
+    if (!userEntity || userEntity.refreshToken !== jwt.payload.refreshToken) {
+      this.cookieService.remove(response, [
+        Definition.Cookies.authToken,
+        Definition.Cookies.authTokenSig,
+      ]);
+      return false;
+    }
+
+    request.context = {
+      ...request.context,
+      getUser: () => this.eventPublisher.mergeObjectContext(userEntity),
+    };
+    return true;
+  }
+}
